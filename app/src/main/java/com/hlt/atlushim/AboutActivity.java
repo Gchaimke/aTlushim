d8bce1536bc49917c43afc165419376397d89909
gchaim <gchaimke@gmail.com>
2020-02-11 15:01:43 +0200

    version 1.01

--- app/src/main/java/com/hlt/atlushim/AboutActivity.java
+++ app/src/main/java/com/hlt/atlushim/AboutActivity.java
@@ -2,13 +2,27 @@ package com.hlt.atlushim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+    PackageInfo pInfo;
+    String version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        try {
+            pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = "V"+pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        TextView appVersionInfo = findViewById(R.id.appVersion);
+        appVersionInfo.setText(version);
     }
 }
